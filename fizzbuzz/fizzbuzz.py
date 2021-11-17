2887e7f27ae8dcd8a8d0157111f51cc46f53eb5c
Pea Tyczynska <CrystalPea@users.noreply.github.com>
2021-11-17 16:49:58 +0000

    fizzbuzz returns buzz when number divisible by 5

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -1,5 +1,7 @@
 def fizzbuzz(number):
     if number % 3 == 0:  # modulo, or remainder after division
         return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
     else:
         return number
--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -7,3 +7,7 @@ def test_fizzbuzz_returns_Fizz_when_number_divisible_by_three():
 
 def test_fizzbuzz_returns_a_number_when_it_is_not_divisble_by_three():
     assert fizzbuzz(4) == 4
+
+
+def test_fizzbuzz_returns_Buzz_when_number_divisible_by_five():
+    assert fizzbuzz(5) == "Buzz"
