16a41cd2a385dccff0327c150b856128de313ad2
Pea Tyczynska <CrystalPea@users.noreply.github.com>
2021-11-17 15:45:43 +0000

    Fizzbuzz returns Fizz for numbers divisible by 3 but not for other numbers

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -1,1 +1,5 @@
-
+def fizzbuzz(number):
+    if number % 3 == 0:  # modulo, or remainder after division
+        return "Fizz"
+    else:
+        return number
--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -1,1 +1,9 @@
+from fizzbuzz.fizzbuzz import fizzbuzz
 
+
+def test_fizzbuzz_returns_Fizz_when_number_divisible_by_three():
+    assert fizzbuzz(3) == "Fizz"
+
+
+def test_fizzbuzz_returns_a_number_when_it_is_not_divisble_by_three():
+    assert fizzbuzz(4) == 4
